82aa3b6cf319917cf710d5cc2da93842812c2784
Big leg <1289941426@qq.com>
2023-06-04 17:50:35 +0800

    done1

--- lab2/DebugExercise/DebugExercise1.java
+++ lab2/DebugExercise/DebugExercise1.java
@@ -6,7 +6,7 @@ package DebugExercise;
  */
 public class DebugExercise1 {
     public static int divideThenRound(int top, int bottom) {
-        int quotient = top / bottom;
+        float quotient = (float) (1.0 * top / bottom);
         int result = Math.round(quotient);
         return result;
     }
